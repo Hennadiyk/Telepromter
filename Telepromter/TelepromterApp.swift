8ca85dfb165cab22992349028a58286bab216483
Hennadiy Kvasov <hennadiyk@gmail.com>
2024-10-07 17:31:00 -0700

    Initial Commit

---
+++ Telepromter/TelepromterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelepromterApp.swift
+//  Telepromter
+//
+//  Created by Hennadiy Kvasov on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TelepromterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TelepromterTests/TelepromterTests.swift
@@ -0,0 +1,17 @@
+//
+//  TelepromterTests.swift
+//  TelepromterTests
+//
+//  Created by Hennadiy Kvasov on 10/7/24.
+//
+
+import Testing
+@testable import Telepromter
+
+struct TelepromterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
